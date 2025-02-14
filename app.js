4b51fcf6c41d91036d02c46b621b99856b518931
Vitor Candido <vitor.candido900@gmail.com>
2025-02-14 09:05:28 -0300

    Inclusao do laco de repeticao

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function sortear(){
     let quantidade = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
+    
 
     let sorteados = [];
     let numero;
@@ -9,10 +10,16 @@ function sortear(){
     for (let i = 0; i < quantidade; i++){
 
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)){ // .includes devolve um booleano se for true avança se for false repete o laço.
+            numero = obterNumeroAleatorio(de, ate)
+        }
+
         sorteados.push(numero);
     }
 
-    alert(sorteados)
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label id="numeroSorteado" class="texto__paragrafo">Números sorteados:  ${sorteados} </label>`;
 }
 
 function obterNumeroAleatorio(min, max){
